42ae1830768dda35bc65ab41402194ab8c119f19
MonaEis <mona.eismann@gmx.de>
2025-05-20 11:52:32 +0200

    Footer, FooterCSS, Beginning FilterButton, Beginning FilterButtonCSS

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,57 @@
+import { Link } from "react-router";
+import "../styles/Footer.css";
+
 const Footer = () => {
-  return <div></div>;
+    return (
+        <section className="footer">
+            <article className="links">
+                <div>
+                    <a href="#start"><h3>MØRENT</h3></a>
+                    <p>
+                        Our vision is to provide convenience and help increase
+                        your sales business.
+                    </p>
+                </div>
+                <div>
+                    <h4>About</h4>
+                    <p>How it works</p>
+                    <p>Featured</p>
+                    <p>Partnership</p>
+                    <p>Business Relation</p>
+                </div>
+                <div>
+                    <h4>Community</h4>
+                    <p>Events</p>
+                    <p>Blog</p>
+                    <p>Podcast</p>
+                    <p>Invite a friend</p>
+                </div>
+                <div className="social">
+                    <h4>Socials</h4>
+                    <Link to="https://github.com/OguzGeylaniYilmaz" target="_blank">
+                        Oguz
+                    </Link>
+                    <Link to="https://github.com/MonaEis" target="_blank">
+                        Mona
+                    </Link>
+                    <Link to="https://github.com/codeshipofmina" target="_blank">
+                        Mina
+                    </Link>
+                    <Link to="https://github.com/ninaraffaela" target="_blank">
+                        Nina
+                    </Link>
+                </div>
+            </article>
+            <div className="line"></div>
+            <article className="copyright">
+                <p>© 2025 Mørent. All rights reserved.</p>
+                <div className="policy">
+                  <p>Privacy & Policy</p>
+                  <p>Terms & Condition</p>
+                </div>
+            </article>
+        </section>
+    );
 };
 
 export default Footer;
